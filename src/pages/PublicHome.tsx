efaf29fbc68eb590e5283dd4d5b568cd34ee5c49
Jaz-spec <jaz.edna@proton.me>
2025-05-09 21:24:54 +0100

    added prettier formatting

--- src/pages/PublicHome.tsx
+++ src/pages/PublicHome.tsx
@@ -1,19 +1,18 @@
-import { Link } from "react-router"
+import { Link } from 'react-router';
 
 export default function PublicHome() {
-    const heading: string = 'Homepage'
-    return <div>
-        <h1>{heading}</h1>
-        <div className="linklist">
-
-        <Link to='/login'>Login</Link>
-        <Link to='/signup'>Sign Up</Link>
-        <Link to='/userhome/all'>User Homepage</Link>
-        <Link to='/userhome/saved'>User Saved</Link>
-        <Link to='/userprofile'>User Profile</Link>
-        <Link to='/error'>Error Page</Link>
-
-        </div>
-
+  const heading: string = 'Homepage';
+  return (
+    <div>
+      <h1>{heading}</h1>
+      <div className="linklist">
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/userhome/all">User Homepage</Link>
+        <Link to="/userhome/saved">User Saved</Link>
+        <Link to="/userprofile">User Profile</Link>
+        <Link to="/error">Error Page</Link>
+      </div>
     </div>
+  );
 }
--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router"
+import { Link } from 'react-router';
 
 export default function UserProfile() {
-    const heading: string = 'UserProfile'
-    return <div>
-        <h1>{heading}</h1>
-        <Link to='/'>Back to Homepage</Link>
+  const heading: string = 'UserProfile';
+  return (
+    <div>
+      <h1>{heading}</h1>
+      <Link to="/">Back to Homepage</Link>
     </div>
+  );
 }
