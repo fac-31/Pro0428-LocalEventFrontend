3af8c99ef42dbc7f13516d6daecb6aea0575a5b7
Jaz-spec <jaz.edna@proton.me>
2025-05-18 12:13:30 +0100

    changed button styling slightly

--- src/components/minor/DirectButton.tsx
+++ src/components/minor/DirectButton.tsx
@@ -16,8 +16,8 @@ export default function DirectButton({
       to={route}
       className={
         pathname === route
-          ? 'text-xl p-1 pl-3 pr-3 ml-2 mr-2 bg-[rgba(225,210,229,0.1)]'
-          : 'text-xl p-1 pl-3 pr-3 ml-2 mr-2 hover:bg-[rgba(225,210,229,0.1)]'
+          ? 'text-xl p-1 pl-5 pr-5 bg-[rgba(225,210,229,0.1)]'
+          : 'text-xl p-1 pl-5 pr-5 hover:bg-[rgba(225,210,229,0.1)]'
       }
     >
       {text}
