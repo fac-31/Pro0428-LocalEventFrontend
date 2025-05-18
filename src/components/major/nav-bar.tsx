2d388346de4bebf8b0dda486e456c40240c0f553
Jaz-spec <jaz.edna@proton.me>
2025-05-18 12:05:36 +0100

    navbar highlights which page yoou are on

--- src/components/major/nav-bar.tsx
+++ src/components/major/nav-bar.tsx
@@ -1,17 +1,31 @@
 import DirectButton from '../minor/DirectButton';
 import { useContext } from 'react';
 import { IdContext } from '../../config/IdContext';
+import { useLocation } from 'react-router';
 
 export default function NavBar() {
+  let pathname: string = useLocation().pathname;
   const { id } = useContext(IdContext);
   if (id) {
     return (
       <div className="flex justify-between border-b-3 input">
         <DirectButton text={'LIGHT MODE'} route={'/userhome'} />
         <div className="flex justify-between">
-          <DirectButton text={'HOMEPAGE'} route={'/userhome'} />
-          <DirectButton text={'MY EVENTS'} route={'/savedevents'} />
-          <DirectButton text={'ACCOUNT'} route={'/userprofile'} />
+          <DirectButton
+            text={'HOMEPAGE'}
+            route={'/userhome'}
+            pathname={pathname}
+          />
+          <DirectButton
+            text={'MY EVENTS'}
+            route={'/savedevents'}
+            pathname={pathname}
+          />
+          <DirectButton
+            text={'ACCOUNT'}
+            route={'/userprofile'}
+            pathname={pathname}
+          />
         </div>
       </div>
     );
--- src/components/minor/DirectButton.tsx
+++ src/components/minor/DirectButton.tsx
@@ -3,13 +3,22 @@ import { Link } from 'react-router';
 interface DirectButtonProps {
   text: string;
   route: string;
+  pathname?: string;
 }
 
-export default function DirectButton({ text, route }: DirectButtonProps) {
+export default function DirectButton({
+  text,
+  route,
+  pathname,
+}: DirectButtonProps) {
   return (
     <Link
       to={route}
-      className="text-xl p-1 ml-2 mr-2 hover:bg-[rgba(225,210,229,0.1)]"
+      className={
+        pathname === route
+          ? 'text-xl p-1 pl-3 pr-3 ml-2 mr-2 bg-[rgba(225,210,229,0.1)]'
+          : 'text-xl p-1 pl-3 pr-3 ml-2 mr-2 hover:bg-[rgba(225,210,229,0.1)]'
+      }
     >
       {text}
     </Link>
