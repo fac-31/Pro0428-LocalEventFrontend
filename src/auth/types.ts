44170613f005db16ddcd83fa5f0c09aeaa978426
Richard Couzens <richardcouzens96@gmail.com>
2025-06-04 23:08:36 +0100

    remove unused auth types file in favor of shared types

--- src/auth/types.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  _id: string;
-  name_first?: string;
-  name_last?: string;
-  email: string;
-  username: string;
-  saved_events: string[];
-  role: 'user' | 'admin';
-}
