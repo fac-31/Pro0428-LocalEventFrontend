5ae90639fdab0542141cb89dae93cfd5f9dcc4de
Jaz-spec <jaz.edna@proton.me>
2025-06-09 16:37:45 +0100

    attempted service route fix

--- src/api/services/events.ts
+++ src/api/services/events.ts
@@ -1,7 +1,7 @@
 export const getEventByMode = async (modes: string[]) => {
   const query = new URLSearchParams({ mode: modes.join(',') }).toString();
 
-  const response = await fetch(`https://the-locals.deno.dev/${query}`);
+  const response = await fetch(`https://the-locals.deno.dev/events?${query}`);
 
   if (!response.ok) {
     throw new Error('Failed to fetch events');
