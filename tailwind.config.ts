13f644a5e44037ad0e00da6ed5207dafd7000192
Jaz-spec <jaz.edna@proton.me>
2025-06-15 21:42:45 +0100

    commented out tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  lightMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--color-background)',
-        text: 'var(--color-text)',
-      },
-    },
-  },
-};
+// export default {
+//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+//   lightMode: 'class',
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: 'var(--color-background)',
+//         text: 'var(--color-text)',
+//       },
+//     },
+//   },
+// };
